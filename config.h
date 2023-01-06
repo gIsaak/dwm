f0dbb1bc963639a39cea4a137569f4a9f1367731
gIsaak <isacco.gobbi@gmail.com>
2023-01-06 22:23:11 +0100

    Patched with systray

--- config.h
+++ config.h
@@ -12,6 +12,10 @@ static unsigned int gappiv          = 10;       /* vert inner gap between window
 static unsigned int gappoh          = 10;       /* horiz outer gap between windows and screen edge */
 static unsigned int gappov          = 10;       /* vert outer gap between windows and screen edge */
 static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
 static int showbar                  = 1;        /* 0 means no bar */
 static int topbar                   = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "HackNerdFont:size=10",
